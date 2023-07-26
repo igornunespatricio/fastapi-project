2934c651e09450918647afbd340ffb0f47d079f5
Igor Nunes Patricio <igornunespatricio@gmail.com>
2023-07-25 21:35:52 -0300

    Adding styles, buttons and bg image

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from fastapi.responses import HTMLResponse
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/assets", StaticFiles(directory="assets"), name="assets")
 
 templates = Jinja2Templates(directory="templates")
 
